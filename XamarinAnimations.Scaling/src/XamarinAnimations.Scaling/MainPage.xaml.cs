f93c136fbefdbd4e0caf4972a9585be98bfcdbe9
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-03-17 11:37:37 -0400

    Added basic scaling effects on tap

--- XamarinAnimations.Scaling/src/XamarinAnimations.Scaling/MainPage.xaml.cs
+++ XamarinAnimations.Scaling/src/XamarinAnimations.Scaling/MainPage.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace XamarinAnimations.Scaling
 {
     public partial class MainPage : ContentPage
     {
+        private double _scale = 0;
         public MainPage()
         {
             InitializeComponent();
+            _scale = Icon.Scale;
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Icon.ScaleTo(_scale * 1.5, 500);
+            await Icon.ScaleTo(_scale, 500);
         }
     }
 }
